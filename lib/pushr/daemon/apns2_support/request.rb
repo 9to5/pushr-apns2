8cc9e8f8b62c9e388f7d5afd23891a35fad8346e
Tom Pesman <tom@9to5.software>
2018-07-21 13:18:08 +0200

    sorted headers, needed?

--- lib/pushr/daemon/apns2_support/request.rb
+++ lib/pushr/daemon/apns2_support/request.rb
@@ -16,10 +16,10 @@ module Pushr
           h = {}
           h['authorization'] = "bearer #{token}"
           h['apns-id'] = notification.apns_id if notification.apns_id
+          h['apns-collapse-id'] = notification.apns_collapse_id if notification.apns_collapse_id
           h['apns-expiration'] = notification.apns_expiration if notification.apns_expiration
           h['apns-priority'] = notification.apns_priority if notification.apns_priority
           h['apns-topic'] = notification.apns_topic if notification.apns_topic
-          h['apns-collapse-id'] = notification.apns_collapse_id if notification.apns_collapse_id
           h
         end
       end
